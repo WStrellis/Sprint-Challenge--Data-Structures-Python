78490dd2a1e731e1bd85da3d0b7306b00a787a6a
WStrellis <wstrellis@gmail.com>
2020-02-28 12:23:49 -0500

    pass 6 tests on Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,16 +5,26 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.current = None
+        self.size = 0
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.size == self.capacity:
+            # remove last item from dll
+            self.storage.remove_from_tail()
+            self.size -= 1
+        # add new item to dll.head
+        self.storage.add_to_head(item)
+        # increment size
+        self.size += 1
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
+        self.current = self.storage.tail
+        while self.current:
+            list_buffer_contents.append(self.current.value)
+            self.current = self.current.prev
 
         return list_buffer_contents
 
