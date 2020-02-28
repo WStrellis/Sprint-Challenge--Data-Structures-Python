95ed59daf69688ee89b2996bb24e261883f8f757
WStrellis <wstrellis@gmail.com>
2020-02-28 13:15:37 -0500

    reduced runtime of names.py to 4.5 seconds

--- names/names.py
+++ names/names.py
@@ -1,26 +1,44 @@
 import time
+from doubly_linked_list import DoublyLinkedList
 
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
+names_1 = f.read().split("\n")  # List containing 10000 names O(n)
 f.close()
 
 f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
+names_2 = f.read().split("\n")  # List containing 10000 names O(n)
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
-
+dll = DoublyLinkedList()
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
 
+for name in names_1:
+    # add item to dll
+    unique_name = dll.add_to_head(name)
+    # add name to duplicates if it was added to dll
+    if unique_name:
+        duplicates.append(unique_name)
+
+for name in names_2:
+    # add item to dll
+    unique_name = dll.add_to_head(name)
+    # add name to duplicates if it was added to dll
+    if unique_name:
+        duplicates.append(unique_name)
+
+
+# for name_1 in names_1:  # O(n)
+#     for name_2 in names_2:  # O(n)
+#         if name_1 == name_2:  # O(1)
+#             duplicates.append(name_1)
+
+#runtime: O(n^2)
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
